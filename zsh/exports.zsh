54b1c3c4103f7917494190cdb4c0ff4261fdfe6f
Reda Bouchaala <bouchaala.reda@gmail.com>
2023-05-05 21:53:07 +0100

    Updated some variables

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -3,8 +3,9 @@
 #
 
 # stuff
-export EDITOR=nvim
-export TERMINAL="tilix"
+export EDITOR="/usr/bin/nvim"
+export TERMINAL="/usr/bin/tilix"
+export BROWSER="/usr/bin/google-chrome-stable"
 
 # Go
 export GOPATH="$HOME/go"
